5d80975c7b1298ded216b90e88c482c1ab33b5f7
Johan W-Schützer <johan.dss@gmail.com>
2016-06-02 22:19:12 +0200

    Added basic dynamic supervisors in infrastructure

---
+++ src/client/infrastructure/line_supervisor.erl
@@ -0,0 +1,23 @@
+-module(line_supervisor).
+-behaviour(supervisor).
+
+-export([start_link/0]).
+-export([init/1]).
+
+start_link() ->
+    supervisor:start_link(infrastructure_line_supervisor, []).
+
+init(_Args) ->
+    SupFlags = { simple_one_for_one
+               , 0
+               , 1
+               },
+    ChildSpecs = [ { line
+                   , {line, start_link, []}
+                   , transient
+                   , 1000
+                   , worker
+                   , [line]
+                   }
+                 ],
+    {ok, {SupFlags, ChildSpecs}}.

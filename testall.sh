023132c72b872e228e9806f83ee85bce3dad344b
Johan W-Schützer <johan.dss@gmail.com>
2016-06-08 09:43:03 +0200

    Dialyzer using wErld.plt instead of default

--- testall.sh
+++ testall.sh
@@ -2,12 +2,13 @@
 echo "====================== Setup env ======================"
 mkdir -p ebin
 mkdir -p etestbin
+PLT=wErld.plt
 echo ""
 
 echo "==================== Setup dialyzer ==================="
-if [ ! -f wErld.plt ];
+if [ ! -f $PLT ];
 then
-  dialyzer --build_plt --apps kernel stdlib erts mnesia --output_plt wErld.plt
+  dialyzer --build_plt --apps kernel stdlib erts mnesia --output_plt $PLT
 fi
 echo ""
 
@@ -16,9 +17,9 @@ erl -make
 echo ""
 
 echo "================ Add wErld to dialyzer ================"
-if [ ! -f wErld.plt ];
+if [ ! -f $PLT ];
 then
-  dialyzer --add_to_plt ebin
+  dialyzer --plt $PLT --add_to_plt ebin
 fi
 echo ""
 
@@ -26,5 +27,5 @@ erl -pa etestbin/ -run werld_tests werld_test -run init stop -noshell
 echo ""
 
 echo "====================== Dialyzing ======================"
-dialyzer -I include/ src --src
+dialyzer --plt $PLT -I include/ src --src
 echo ""

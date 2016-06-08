5d74e18e11382b50b0f3c5f606068f01c585acf4
Johan W-Schützer <johan.dss@gmail.com>
2016-06-08 08:58:46 +0200

    testall creates bindirs if they don't exist

--- testall.sh
+++ testall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 echo "======================= Compile ======================="
+mkdir -p ebin
+mkdir -p etestbin
 erl -make
 echo ""
 erl -pa etestbin/ -run werld_tests werld_test -run init stop -noshell

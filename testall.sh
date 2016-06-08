3c2ed00a49c0efb2afc6c5049526bdfb71d5d093
Johan W-Schützer <johan.dss@gmail.com>
2016-06-08 09:28:46 +0200

    Better info from testall

--- testall.sh
+++ testall.sh
@@ -1,17 +1,30 @@
 #!/bin/bash -e
-echo "======================= Compile ======================="
+echo "====================== Setup env ======================"
 mkdir -p ebin
 mkdir -p etestbin
+echo ""
+
+echo "==================== Setup dialyzer ==================="
 if [ ! -f wErld.plt ];
 then
   dialyzer --build_plt --apps kernel stdlib erts mnesia eunit --output_plt wErld.plt
-  erl -make
-  dialyzer --add_to_plt ebin
-else
-  erl -make
 fi
 echo ""
+
+echo "======================= Compile ======================="
+erl -make
+echo ""
+
+echo "================ Add wErld to dialyzer ================"
+if [ ! -f wErld.plt ];
+then
+  dialyzer --add_to_plt ebin
+fi
+echo ""
+
 erl -pa etestbin/ -run werld_tests werld_test -run init stop -noshell
 echo ""
+
 echo "====================== Dialyzing ======================"
 dialyzer -I include/ src --src
+echo ""

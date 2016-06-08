c46b52cc7238cef786831f849ca10162371bcbd3
Johan W-Schützer <johan.dss@gmail.com>
2016-06-08 09:23:41 +0200

    Testall creates plt if not exists

--- testall.sh
+++ testall.sh
@@ -2,7 +2,14 @@
 echo "======================= Compile ======================="
 mkdir -p ebin
 mkdir -p etestbin
-erl -make
+if [ ! -f wErld.plt ];
+then
+  dialyzer --build_plt --apps kernel stdlib erts mnesia eunit --output_plt wErld.plt
+  erl -make
+  dialyzer --add_to_plt ebin
+else
+  erl -make
+fi
 echo ""
 erl -pa etestbin/ -run werld_tests werld_test -run init stop -noshell
 echo ""

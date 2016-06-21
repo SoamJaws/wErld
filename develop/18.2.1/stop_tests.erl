a17a23471563575209b19977918712ef54bb8bad
Travis <travis@travis-ci.org>
2016-06-21 06:49:12 +0000

    Travis build 55 pushed to gh-pages

--- develop/18.2.1/stop_tests.erl
+++ develop/18.2.1/stop_tests.erl
@@ -93,6 +93,9 @@ vehicle_check_out_test() ->
   {ok, V2} = gen_server_mock:start_link(v2),
   
   ?assertEqual(ok, stop:?PASSENGER_CHECK_IN(Stop, P1)),
+  stop:?PASSENGER_CHECK_OUT(Stop, P1, false),
+
+  ?assertEqual(ok, stop:?PASSENGER_CHECK_IN(Stop, P1)),
   ?assertEqual(ok, stop:?PASSENGER_CHECK_IN(Stop, P2)),
   ?assertEqual(ok, stop:?PASSENGER_CHECK_IN(Stop, P3)),
 
@@ -115,6 +118,40 @@ vehicle_check_out_test() ->
   stop:?PASSENGER_CHECK_OUT(Stop, P3, true),
   stop:?VEHICLE_CHECK_OUT(Stop, V1, true),
   stop:?VEHICLE_CHECK_OUT(Stop, V2, true), %% Exercise check out when no vehicles queued
+
+  ?assert(gen_server_mock:validate(P1)),
+  ?assert(gen_server_mock:validate(P2)),
+  ?assert(gen_server_mock:validate(P3)),
+  ?assert(gen_server_mock:validate(V1)),
+  ?assert(gen_server_mock:validate(V2)),
+  gen_server_mock:stop(P1),
+  gen_server_mock:stop(P2),
+  gen_server_mock:stop(P3),
+  gen_server_mock:stop(V1),
+  gen_server_mock:stop(V2),
+  stop:stop(Stop).
+
+state_test() ->
+  {ok, Stop} = stop:start_link(stop1, 3),
+  {ok, P1} = gen_server_mock:start_link(p1),
+  {ok, P2} = gen_server_mock:start_link(p2),
+  {ok, P3} = gen_server_mock:start_link(p3),
+  {ok, V1} = gen_server_mock:start_link(v1),
+  {ok, V2} = gen_server_mock:start_link(v2),
+  
+  ?assertEqual(ok, stop:?PASSENGER_CHECK_IN(Stop, P1)),
+  ?assertEqual(ok, stop:?PASSENGER_CHECK_IN(Stop, P2)),
+  ?assertEqual(ok, stop:?PASSENGER_CHECK_IN(Stop, P3)),
+
+  gen_server_mock:expect_cast(V1, {checkin_ok, Stop, false, Stop}),
+  gen_server_mock:expect_call(P1, {vehicle_checked_in, V1}, ok),
+  gen_server_mock:expect_call(P2, {vehicle_checked_in, V1}, ok),
+  gen_server_mock:expect_call(P3, {vehicle_checked_in, V1}, ok),
+
+  stop:?VEHICLE_CHECK_IN(Stop, V1, true),
+  stop:?VEHICLE_CHECK_IN(Stop, V2, true),
+
+  ?assertMatch(#stop_state{id=stop1, capacity=3, currentVehicle=V1, passengers=[P1,P2,P3], vehicleQueue=[V2]}, stop:state(Stop)),
 
   ?assert(gen_server_mock:validate(P1)),
   ?assert(gen_server_mock:validate(P2)),

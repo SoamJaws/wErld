db00fac8ef57a11d171dc89b929fa887e21c64ae
Travis <travis@travis-ci.org>
2016-06-25 19:47:28 +0000

    Travis build 69 pushed to gh-pages

--- develop/18.0/stop_tests.erl
+++ develop/18.0/stop_tests.erl
@@ -58,28 +58,33 @@ vehicle_check_in_test() ->
   {ok, P1} = gen_server_mock:start_link(p1, strict),
   {ok, P2} = gen_server_mock:start_link(p2, strict),
   {ok, P3} = gen_server_mock:start_link(p3, strict),
+  {ok, P4} = gen_server_mock:start_link(p4, strict),
   {ok, V1} = gen_server_mock:start_link(v1, strict),
   
   ?assertEqual(ok, stop:?PASSENGER_CHECK_IN(Stop, P1)),
   ?assertEqual(ok, stop:?PASSENGER_CHECK_IN(Stop, P2)),
 
-  gen_server_mock:expect_cast(V1, {?CHECKIN_OK, Stop, 2, false, Stop}),
+  gen_server_mock:expect_cast(V1, {?CHECKIN_OK, Stop, 1, false, Stop}),
   gen_server_mock:expect_call(P1, {vehicle_checked_in, V1}, true),
-  gen_server_mock:expect_call(P2, {vehicle_checked_in, V1}, true),
+  gen_server_mock:expect_call(P2, {vehicle_checked_in, V1}, false),
 
   stop:?VEHICLE_CHECK_IN(Stop, V1, true),
 
   gen_server_mock:expect_call(P3, {vehicle_checked_in, V1}, true),
+  gen_server_mock:expect_call(P4, {vehicle_checked_in, V1}, false),
   gen_server_mock:expect_cast(V1, {?INCREMENT_BOARDING_PASSENGER, false, Stop}),
   ?assertEqual(ok, stop:?PASSENGER_CHECK_IN(Stop, P3)),
+  ?assertEqual(ok, stop:?PASSENGER_CHECK_IN(Stop, P4)),
 
   ?assert(gen_server_mock:validate(P1)),
   ?assert(gen_server_mock:validate(P2)),
   ?assert(gen_server_mock:validate(P3)),
+  ?assert(gen_server_mock:validate(P4)),
   ?assert(gen_server_mock:validate(V1)),
   gen_server_mock:stop(P1),
   gen_server_mock:stop(P2),
   gen_server_mock:stop(P3),
+  gen_server_mock:stop(P4),
   gen_server_mock:stop(V1),
   stop:stop(Stop).
 

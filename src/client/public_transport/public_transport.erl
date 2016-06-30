5b553a7e64336d30ca19fa095f5f5e38d39318dd
Johan W-Schützer <johan.dss@gmail.com>
2016-06-30 17:55:06 +0200

    fun specs progress

--- src/client/public_transport/public_transport.erl
+++ src/client/public_transport/public_transport.erl
@@ -20,21 +20,26 @@
 
 %% Public API
 
+-spec start_link() -> {ok, pid()} | ignore | {error, {already_started, pid()} | term()}.
 start_link() ->
   gen_server:start_link(?MODULE, [], []).
 
+-spec stop(pid()) -> ok.
 stop(Pid) ->
   gen_server:call(Pid, stop).
 
+-spec state(pid()) -> public_transport_state().
 state(Pid) ->
   gen_server:call(Pid, state).
 
+-spec ?GET_ROUTE(pid(), pid(), pid()) -> {[{pid(), pid(), pid()}], pos_integer()} | none.
 ?GET_ROUTE(Pid, From, To) ->
   gen_server:call(Pid, {?GET_ROUTE, From, To}).
 
 
 %% gen_server
 
+-spec init([]) -> {ok, public_transport_state()}.
 init([]) ->
   {ok, {{stops, StopSpecs}, {lines, LineSpecs}}} = file:script(?INFRASTRUCTURE_DATA_PATH),
   %%TODO Start a stop for each stopspec.
@@ -43,7 +48,9 @@ init([]) ->
   Lines = ok,
   {ok, #public_transport_state{lines=Lines}}.
 
-
+-spec handle_call({?GET_ROUTE, pid(), pid()}, {pid(), any()}, public_transport_state()) -> {reply, {[{pid(), pid(), pid()}], pos_integer()} | none, public_transport_state()}
+      ;          (stop,                       {pid(), any()}, public_transport_state()) -> {stop, normal, stopped, stop_state()}
+      ;          (state,                      {pid(), any()}, public_transport_state()) -> {reply, public_transport_state(), public_transport_state()}.
 handle_call({?GET_ROUTE, From, To}, _From, Lines) ->
   Reply = get_route_helper(From, To, Lines),
   {reply, Reply, Lines};
@@ -55,18 +62,22 @@ handle_call(stop, _From, Lines) ->
   {stop, normal, stopped, Lines}.
 
 
+-spec handle_cast(any(), public_transport_state()) -> {noreply, public_transport_state()}.
 handle_cast(_, State) ->
   {noreply, State}.
 
 
+-spec handle_info(timeout | any(), public_transport_state()) -> {noreply, public_transport_state()}.
 handle_info(_Info, State) ->
   {noreply, State}.
 
 
+-spec terminate(normal | shutdown | {shutdown, any()} | any(), stop_state()) -> ok.
 terminate(_Reason, _State) ->
   ok.
 
 
+-spec code_change(term() | {down, term()}, stop_state(), term()) -> {ok, stop_state()}.
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
 
@@ -75,6 +86,7 @@ code_change(_OldVsn, State, _Extra) ->
 
 %% Instructionsformat: list of tuples {[{Line, Target, Destination}, {Line, Target, Destination}...], Dur}
 %% Citizen goes from From to Destination by line in the Target direction, repeat until arrived at To
+-spec get_route_helper(pid(), pid(), [pid()]) -> {[{pid(), pid(), pid()}], pos_integer()} | none.
 get_route_helper(From, To, AllLines) ->
   ToLines = [Line || Line <- AllLines, line:?CONTAINS_STOP(Line, To)],
   spawn(public_transport, get_route_concurrent, [From, To, ToLines, {[], 0}, [], AllLines, self()]),
@@ -83,6 +95,7 @@ get_route_helper(From, To, AllLines) ->
   end.
 
 
+-spec get_route_concurrent(pid(), pid(), [pid()], {[{pid(), pid(), pid()}], non_neg_integer()}, [pid()], [pid()], pid()) -> {[{pid(), pid(), pid()}], pos_integer()} | none.
 get_route_concurrent(From, To, ToLines, {Route, Dur}, VisitedStops, AllLines, Invoker) ->
   FromLines = [Line || Line <- AllLines, line:?CONTAINS_STOP(Line, From)],
   IntersectingLines = get_intersecting_lines(FromLines, ToLines),
@@ -104,9 +117,11 @@ get_route_concurrent(From, To, ToLines, {Route, Dur}, VisitedStops, AllLines, In
   end.
 
 
+-spec spawn_get_route_calls([pid()], pid(), [pid()], {[{pid(), pid(), pid()}], pos_integer()} | none, [pid()], [pid()]) -> [{[{pid(), pid(), pid()}], pos_integer()}].
 spawn_get_route_calls(Neighbors, To, ToLines, Route, VisitedStops, AllLines) ->
   spawn_get_route_calls(Neighbors, To, ToLines, Route, VisitedStops, AllLines, 0).
 
+-spec spawn_get_route_calls([pid()], pid(), [pid()], {[{pid(), pid(), pid()}], pos_integer()} | none, [pid()], [pid()], non_neg_integer()) -> [{[{pid(), pid(), pid()}], pos_integer()}].
 spawn_get_route_calls([], _To, _ToLines, _Route, _VisitedStops, _AllLines, NoCalls) ->
   receive_routes(NoCalls);
 spawn_get_route_calls([Neighbor|Neighbors], To, ToLines, {Route, TotalDur}, VisitedStops, AllLines, NoCalls) ->
@@ -120,10 +135,11 @@ spawn_get_route_calls([Neighbor|Neighbors], To, ToLines, {Route, TotalDur}, Visi
       spawn_get_route_calls(Neighbors, To, ToLines, {Route, TotalDur}, VisitedStops, AllLines, NoCalls)
   end.
 
-
+-spec receive_routes(non_neg_integer()) -> [{[{pid(), pid(), pid()}], pos_integer()}].
 receive_routes(NoCalls) ->
   receive_routes(NoCalls, []).
 
+-spec receive_routes(non_neg_integer(), [{[{pid(), pid(), pid()}], pos_integer()}) -> [{[{pid(), pid(), pid()}], pos_integer()}].
 receive_routes(0, Routes) -> Routes;
 receive_routes(NoCalls, Routes) ->
   receive
@@ -133,10 +149,11 @@ receive_routes(NoCalls, Routes) ->
       receive_routes(NoCalls-1, [Route|Routes])
   end.
 
-
+-spec get_best_route([{[{pid(), pid(), pid()}], pos_integer()}]) -> {[{pid(), pid(), pid()}], pos_integer()}.
 get_best_route(Routes) ->
   get_best_route(Routes, {none, -1}).
 
+-spec get_best_route([{[{pid(), pid(), pid()}], pos_integer()}], {[{pid(), pid(), pid()}], integer()}) -> {[{pid(), pid(), pid()}], pos_integer()}.
 get_best_route([], BestRoute) -> BestRoute;
 get_best_route([Route|Routes], {none, -1}) ->
   get_best_route(Routes, Route);
@@ -150,9 +167,11 @@ get_best_route([{Route, Dur}|Routes], {BestRoute, BestDur}) ->
 
 
 %% [{FromLine, ToLine, IntersectingStop}]
+-spec get_intersecting_lines(pid(), pid()) -> [{pid(), pid(), pid()}].
 get_intersecting_lines(FromLines, ToLines) ->
   get_intersecting_lines([{FromLine, ToLine, line:?GET_INTERSECTION(FromLine, ToLine)} || FromLine <- FromLines, ToLine <- ToLines]).
 
+-spec get_intersecting_lines([{pid(), pid(), pid()}]) -> [{pid(), pid(), pid()}].
 get_intersecting_lines([]) -> [];
 get_intersecting_lines([{_,_,none}|IntersectingLines]) ->
   get_intersecting_lines(IntersectingLines);
@@ -160,9 +179,11 @@ get_intersecting_lines([IntersectingLine|IntersectingLines]) ->
   [IntersectingLine|get_intersecting_lines(IntersectingLines)].
 
 
+-spec get_best_intersecting_lines([{pid(), pid(), pid(), pos_integer()}]) -> {pid(), pid(), pid(), pos_integer()}.
 get_best_intersecting_lines(IntersectingLinesWithDurations) ->
   get_best_intersecting_lines(IntersectingLinesWithDurations, {none, none, none, 0}).
 
+-spec get_best_intersecting_lines([{pid(), pid(), pid(), pos_integer()}], {pid() | none, pid() | none, pid() | none, non_neg_integer()}) -> {pid(), pid(), pid(), pos_integer()}.
 get_best_intersecting_lines([], BestIntersectingLines) -> BestIntersectingLines;
 get_best_intersecting_lines([{FromLine, ToLine, IntersectingStop, Dur}|IntersectingLinesWithDurations], {BestFromLine, BestToLine, BestIntersectingStop, BestDur}) ->
   if

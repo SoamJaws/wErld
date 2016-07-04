09f5602db77d6c013f2350856b4020aa877298ec
Travis <travis@travis-ci.org>
2016-07-04 07:08:55 +0000

    Travis build 110 pushed to gh-pages

--- develop/18.2.1/public_transport_tests.erl
+++ develop/18.2.1/public_transport_tests.erl
@@ -2,4 +2,5 @@
 -include_lib("eunit/include/eunit.hrl").
 
 public_transport_test() ->
+  {ok, _PublicTransportSupervisor} = public_transport_supervisor:start_link(),
   {ok, _PublicTransport} = public_transport:start_link().

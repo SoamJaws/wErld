db776385b5f898bb15c00b0d534b9eac90ec4913
Travis <travis@travis-ci.org>
2016-07-04 15:24:04 +0000

    Travis build 116 pushed to gh-pages

--- develop/18.1/public_transport_tests.erl
+++ develop/18.1/public_transport_tests.erl
@@ -3,4 +3,5 @@
 
 public_transport_test() ->
   {ok, _PublicTransportSupervisor} = public_transport_supervisor:start_link(),
-  {ok, _PublicTransport} = public_transport:start_link().
+  {ok, _PublicTransport} = public_transport:start_link(),
+  ?debugFmt("~p~n", [supervisor:which_children({global, stop_supervisor})]).

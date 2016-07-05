fa67656b03bbff1850ddaca7d74a562d573be00d
Travis <travis@travis-ci.org>
2016-07-05 06:29:56 +0000

    Travis build 119 pushed to gh-pages

--- develop/18.2/public_transport_tests.erl
+++ develop/18.2/public_transport_tests.erl
@@ -1,7 +1,9 @@
 -module(public_transport_tests).
+-include("public_transport.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
 public_transport_test() ->
   {ok, _PublicTransportSupervisor} = public_transport_supervisor:start_link(),
-  {ok, _PublicTransport} = public_transport:start_link(),
-  ?debugFmt("~p~n", [supervisor:which_children({global, stop_supervisor})]).
+  {ok, PublicTransport} = public_transport:start_link(),
+  Route = public_transport:?GET_ROUTE(PublicTransport, a, g),
+  ?debugFmt("~p~n", [Route]).

39232b0a97608da6b17ac8a85b0a63e43e1ff7f8
Travis <travis@travis-ci.org>
2016-07-05 15:27:42 +0000

    Travis build 134 pushed to gh-pages

--- develop/18.2/public_transport_tests.erl
+++ develop/18.2/public_transport_tests.erl
@@ -5,5 +5,5 @@
 public_transport_test() ->
   {ok, _PublicTransportSupervisor} = public_transport_supervisor:start_link(),
   {ok, PublicTransport} = public_transport:start_link(),
-  Route = public_transport:?GET_ROUTE(PublicTransport, a, g),
+  Route = public_transport:?GET_ROUTE(PublicTransport, a, o),
   ?debugFmt("~p~n", [Route]).

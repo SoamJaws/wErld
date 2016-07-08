2f554f6bd9cecfc9f4f0164e627dbcfcbf19c3d1
Travis <travis@travis-ci.org>
2016-07-08 05:27:48 +0000

    Travis build 143 pushed to gh-pages

--- develop/18.0/stop.erl
+++ develop/18.0/stop.erl
@@ -4,16 +4,15 @@
 -behaviour(gen_server).
 
 %% Public API
--export([ start_link/1
-        , stop/1
-        , state/1
-        , ?PASSENGER_CHECK_IN/2
+-export([ ?PASSENGER_CHECK_IN/2
         , ?PASSENGER_CHECK_OUT/3
         , ?VEHICLE_CHECK_IN/3
-        , ?VEHICLE_CHECK_OUT/3]).
+        , ?VEHICLE_CHECK_OUT/3
+        , state/1]).
 
 %% gen_server
--export([ init/1
+-export([ start_link/1
+        , init/1
         , handle_call/3
         , handle_cast/2
         , handle_info/2
@@ -23,36 +22,33 @@
 
 %% Public API
 
--spec start_link(atom()) -> {ok, pid()} | ignore | {error, {already_started, pid()} | term()}.
-start_link(Id) ->
-  gen_server:start_link(?MODULE, Id, []).
+-spec ?PASSENGER_CHECK_IN(stop(), pid()) -> ok | {nok, nonempty_string()}.
+?PASSENGER_CHECK_IN(?RECIPENT, Passenger) ->
+  gen_server:call(Pid, {?PASSENGER_CHECK_IN, Passenger}).
 
--spec stop(pid()) -> ok.
-stop(Pid) ->
-  gen_server:call(Pid, stop).
+-spec ?PASSENGER_CHECK_OUT(stop(), pid(), boolean())-> ok.
+?PASSENGER_CHECK_OUT(?RECIPENT, Passenger, BlockCaller) ->
+  gen_server_utils:cast(Pid, {?PASSENGER_CHECK_OUT, Passenger}, BlockCaller).
+
+-spec ?VEHICLE_CHECK_IN(stop(), vehicle(), boolean()) -> ok.
+?VEHICLE_CHECK_IN(?RECIPENT, Vehicle, BlockCaller) ->
+  gen_server_utils:cast(Pid, {?VEHICLE_CHECK_IN, Vehicle}, BlockCaller).
+
+-spec ?VEHICLE_CHECK_OUT(stop(), vehicle(), boolean()) -> ok.
+?VEHICLE_CHECK_OUT(?RECIPENT, Vehicle, BlockCaller) ->
+  gen_server_utils:cast(Pid, {?VEHICLE_CHECK_OUT, Vehicle}, BlockCaller).
 
 -spec state(pid()) -> stop_state().
 state(Pid) ->
   gen_server:call(Pid, state).
 
--spec ?PASSENGER_CHECK_IN(pid(), pid()) -> ok | {nok, nonempty_string()}.
-?PASSENGER_CHECK_IN(Pid, Passenger) ->
-  gen_server:call(Pid, {?PASSENGER_CHECK_IN, Passenger}).
-
--spec ?PASSENGER_CHECK_OUT(pid(), pid(), boolean())-> ok.
-?PASSENGER_CHECK_OUT(Pid, Passenger, BlockCaller) ->
-  gen_server_utils:cast(Pid, {?PASSENGER_CHECK_OUT, Passenger}, BlockCaller).
-
--spec ?VEHICLE_CHECK_IN(pid(), pid(), boolean()) -> ok.
-?VEHICLE_CHECK_IN(Pid, Vehicle, BlockCaller) ->
-  gen_server_utils:cast(Pid, {?VEHICLE_CHECK_IN, Vehicle}, BlockCaller).
-
--spec ?VEHICLE_CHECK_OUT(pid(), pid(), boolean()) -> ok.
-?VEHICLE_CHECK_OUT(Pid, Vehicle, BlockCaller) ->
-  gen_server_utils:cast(Pid, {?VEHICLE_CHECK_OUT, Vehicle}, BlockCaller).
-
 
 %% gen_server
+
+-spec start_link(atom()) -> {ok, pid()} | ignore | {error, {already_started, pid()} | term()}.
+start_link(Id) ->
+  gen_server:start_link(?MODULE, Id, []).
+
 
 -spec init(atom()) -> {ok, stop_state()}.
 init(Id) ->
@@ -62,7 +58,6 @@ init(Id) ->
 
 
 -spec handle_call({?PASSENGER_CHECK_IN, pid()}, {pid(), any()}, stop_state()) -> {reply, ok | {nok, string()}, stop_state()}
-      ;          (stop,                         {pid(), any()}, stop_state()) -> {stop, normal, stopped, stop_state()}
       ;          (state,                        {pid(), any()}, stop_state()) -> {reply, stop_state(), stop_state()}.
 handle_call({?PASSENGER_CHECK_IN, Passenger}, _From, State) ->
   Passengers = State#stop_state.passengers,
@@ -87,16 +82,13 @@ handle_call({?PASSENGER_CHECK_IN, Passenger}, _From, State) ->
       {reply, ok, State#stop_state{passengers=Passengers++[Passenger]}}
   end;
 
-handle_call(stop, _From, State) ->
-  {stop, normal, stopped, State};
-
 handle_call(state, _From, State) ->
   {reply, State, State}.
 
 
 -spec handle_cast({?PASSENGER_CHECK_OUT, pid(), boolean(), pid()}, stop_state()) -> {noreply, stop_state()}
-      ;          ({?VEHICLE_CHECK_IN, pid(), boolean(), pid()}, stop_state()) -> {noreply, stop_state()}
-      ;          ({?VEHICLE_CHECK_OUT, pid(), boolean(), pid()}, stop_state()) -> {noreply, stop_state()}.
+      ;          ({?VEHICLE_CHECK_IN, vehicle(), boolean(), pid()}, stop_state()) -> {noreply, stop_state()}
+      ;          ({?VEHICLE_CHECK_OUT, vehicle(), boolean(), pid()}, stop_state()) -> {noreply, stop_state()}.
 handle_cast({?PASSENGER_CHECK_OUT, Passenger, NotifyCaller, Caller}, State) ->
   Passengers = lists:delete(Passenger, State#stop_state.passengers),
   gen_server_utils:notify_caller(NotifyCaller, Caller),
@@ -150,11 +142,11 @@ code_change(_OldVsn, State, _Extra) ->
 
 %% Backend
 
--spec notify_vehicle_checked_in([pid()], pid()) -> non_neg_integer().
+-spec notify_vehicle_checked_in([pid()], vehicle()) -> non_neg_integer().
 notify_vehicle_checked_in(Passengers, Vehicle) ->
   notify_vehicle_checked_in(Passengers, Vehicle, 0).
 
--spec notify_vehicle_checked_in([pid()], pid(), non_neg_integer()) -> non_neg_integer().
+-spec notify_vehicle_checked_in([pid()], vehicle(), non_neg_integer()) -> non_neg_integer().
 notify_vehicle_checked_in([], _Vehicle, BoardingPassengers) -> BoardingPassengers;
 notify_vehicle_checked_in([Passenger|Passengers], Vehicle, BoardingPassengers) ->
   WillBoard = citizen:vehicle_checked_in(Passenger, Vehicle),

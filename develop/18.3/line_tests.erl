2e5e32265f97fc61a7e91ce8edbe41fc4e238630
Travis <travis@travis-ci.org>
2016-07-13 15:30:02 +0000

    Travis build 159 pushed to gh-pages

--- develop/18.3/line_tests.erl
+++ develop/18.3/line_tests.erl
@@ -3,12 +3,13 @@
 -include_lib("eunit/include/eunit.hrl").
 
 get_next_stop_test() ->
+  line_supervisor:start_link(),
   {ok, Stop1} = stop:start_link(stop1),
   {ok, Stop2} = stop:start_link(stop2),
   {ok, Stop3} = stop:start_link(stop3),
   Dur1_2 = 10,
   Dur2_3 = 20,
-  {ok, Line} = line:start_link(1, [Stop1, Dur1_2, Stop2, Dur2_3, Stop3], bus),
+  Line = line_supervisor:start_line(1, [Stop1, Dur1_2, Stop2, Dur2_3, Stop3], bus),
 
   ?assertEqual(line:?GET_NEXT_STOP(Line, Stop3, Stop1), {Stop2, Dur1_2}),
   ?assertEqual(line:?GET_NEXT_STOP(Line, Stop3, Stop2), {Stop3, Dur2_3}),
@@ -20,9 +21,10 @@ get_next_stop_test() ->
   stop:stop(Stop1),
   stop:stop(Stop2),
   stop:stop(Stop3),
-  line:stop(Line).
+  line_supervisor:stop_line(Line).
 
 get_neighbors_test() ->
+  line_supervisor:start_link(),
   {ok, Stop1} = stop:start_link(stop1),
   {ok, Stop2} = stop:start_link(stop2),
   {ok, Stop3} = stop:start_link(stop3),
@@ -32,7 +34,7 @@ get_neighbors_test() ->
   Dur2_3 = 11,
   Dur3_4 = 13,
   Dur4_5 = 17,
-  {ok, Line} = line:start_link(1, [Stop1, Dur1_2, Stop2, Dur2_3, Stop3, Dur3_4, Stop4, Dur4_5, Stop5], bus),
+  Line = line_supervisor:start_line(1, [Stop1, Dur1_2, Stop2, Dur2_3, Stop3, Dur3_4, Stop4, Dur4_5, Stop5], bus),
   
   ?assertEqual(line:?GET_NEIGHBORS(Line, Stop1), [{Stop2, Dur1_2, Stop5, Line}]),
   ?assertEqual(line:?GET_NEIGHBORS(Line, Stop2), [{Stop1, Dur1_2, Stop1, Line}, {Stop3, Dur2_3, Stop5, Line}]),
@@ -45,15 +47,16 @@ get_neighbors_test() ->
   stop:stop(Stop3),
   stop:stop(Stop4),
   stop:stop(Stop5),
-  line:stop(Line).
+  line_supervisor:stop_line(Line).
 
 get_other_end_test() ->
+  line_supervisor:start_link(),
   {ok, Stop1} = stop:start_link(stop1),
   {ok, Stop2} = stop:start_link(stop2),
   {ok, Stop3} = stop:start_link(stop3),
   Dur1_2 = 10,
   Dur2_3 = 20,
-  {ok, Line} = line:start_link(1, [Stop1, Dur1_2, Stop2, Dur2_3, Stop3], bus),
+  Line = line_supervisor:start_line(1, [Stop1, Dur1_2, Stop2, Dur2_3, Stop3], bus),
 
   ?assertEqual(line:?GET_OTHER_END(Line, Stop1), Stop3),
   ?assertEqual(line:?GET_OTHER_END(Line, Stop3), Stop1),
@@ -61,16 +64,17 @@ get_other_end_test() ->
   stop:stop(Stop1),
   stop:stop(Stop2),
   stop:stop(Stop3),
-  line:stop(Line).
+  line_supervisor:stop_line(Line).
 
 contains_stop_test() ->
+  line_supervisor:start_link(),
   {ok, Stop1} = stop:start_link(stop1),
   {ok, Stop2} = stop:start_link(stop2),
   {ok, Stop3} = stop:start_link(stop3),
   {ok, Stop4} = stop:start_link(stop4),
   Dur1_2 = 10,
   Dur2_3 = 20,
-  {ok, Line} = line:start_link(1, [Stop1, Dur1_2, Stop2, Dur2_3, Stop3], bus),
+  Line = line_supervisor:start_line(1, [Stop1, Dur1_2, Stop2, Dur2_3, Stop3], bus),
 
   ?assertEqual(true, line:?CONTAINS_STOP(Line, Stop1)),
   ?assertEqual(true, line:?CONTAINS_STOP(Line, Stop2)),
@@ -81,9 +85,10 @@ contains_stop_test() ->
   stop:stop(Stop2),
   stop:stop(Stop3),
   stop:stop(Stop4),
-  line:stop(Line).
+  line_supervisor:stop_line(Line).
 
 get_duration_test() ->
+  line_supervisor:start_link(),
   {ok, Stop1} = stop:start_link(stop1),
   {ok, Stop2} = stop:start_link(stop2),
   {ok, Stop3} = stop:start_link(stop3),
@@ -93,7 +98,7 @@ get_duration_test() ->
   Dur2_3 = 11,
   Dur3_4 = 13,
   Dur4_5 = 17,
-  {ok, Line} = line:start_link(1, [Stop1, Dur1_2, Stop2, Dur2_3, Stop3, Dur3_4, Stop4, Dur4_5, Stop5], bus),
+  Line = line_supervisor:start_line(1, [Stop1, Dur1_2, Stop2, Dur2_3, Stop3, Dur3_4, Stop4, Dur4_5, Stop5], bus),
 
   ?assertEqual(line:?GET_DURATION(Line, Stop1, Stop2), Dur1_2),
   ?assertEqual(line:?GET_DURATION(Line, Stop1, Stop3), Dur1_2 + Dur2_3),
@@ -122,16 +127,17 @@ get_duration_test() ->
   stop:stop(Stop3),
   stop:stop(Stop4),
   stop:stop(Stop5),
-  line:stop(Line).
+  line_supervisor:stop_line(Line).
 
 is_end_stop_test() ->
+  line_supervisor:start_link(),
   {ok, Stop1} = stop:start_link(stop1),
   {ok, Stop2} = stop:start_link(stop2),
   {ok, Stop3} = stop:start_link(stop3),
   Dur1_2 = 10,
   Dur2_3 = 20,
 
-  {ok, Line} = line:start_link(1, [Stop1, Dur1_2, Stop2, Dur2_3, Stop3], bus),
+  Line = line_supervisor:start_line(1, [Stop1, Dur1_2, Stop2, Dur2_3, Stop3], bus),
   ?assertEqual(true, line:?IS_END_STOP(Line, Stop1)),
   ?assertEqual(false, line:?IS_END_STOP(Line, Stop2)),
   ?assertEqual(true, line:?IS_END_STOP(Line, Stop3)),
@@ -139,9 +145,10 @@ is_end_stop_test() ->
   stop:stop(Stop1),
   stop:stop(Stop2),
   stop:stop(Stop3),
-  line:stop(Line).
+  line_supervisor:stop_line(Line).
 
 get_intersection_test() ->
+  line_supervisor:start_link(),
   {ok, Stop1} = stop:start_link(stop1),
   {ok, Stop2} = stop:start_link(stop2),
   {ok, Stop3} = stop:start_link(stop3),
@@ -151,10 +158,10 @@ get_intersection_test() ->
   {ok, Stop7} = stop:start_link(stop7),
   {ok, Stop8} = stop:start_link(stop8),
   {ok, Stop9} = stop:start_link(stop9),
-  {ok, Line1} = line:start_link(1, [Stop1, 5, Stop2, 5, Stop3], bus),
-  {ok, Line2} = line:start_link(2, [Stop4, 5, Stop2, 5, Stop5], bus),
-  {ok, Line3} = line:start_link(3, [Stop1, 5, Stop6, 5, Stop7], bus),
-  {ok, Line4} = line:start_link(4, [Stop8, 5, Stop9, 5, Stop7], bus),
+  Line1 = line_supervisor:start_line(1, [Stop1, 5, Stop2, 5, Stop3], bus),
+  Line2 = line_supervisor:start_line(2, [Stop4, 5, Stop2, 5, Stop5], bus),
+  Line3 = line_supervisor:start_line(3, [Stop1, 5, Stop6, 5, Stop7], bus),
+  Line4 = line_supervisor:start_line(4, [Stop8, 5, Stop9, 5, Stop7], bus),
 
   ?assertEqual(line:?GET_INTERSECTION(Line1, Line2), Stop2),
   ?assertEqual(line:?GET_INTERSECTION(Line2, Line1), Stop2),
@@ -179,25 +186,26 @@ get_intersection_test() ->
   stop:stop(Stop7),
   stop:stop(Stop8),
   stop:stop(Stop9),
-  line:stop(Line1),
-  line:stop(Line2),
-  line:stop(Line3),
-  line:stop(Line4).
+  line_supervisor:stop_line(Line1),
+  line_supervisor:stop_line(Line2),
+  line_supervisor:stop_line(Line3),
+  line_supervisor:stop_line(Line4).
 
 get_number_test() ->
   {ok, Stop1} = stop:start_link(stop1),
   {ok, Stop2} = stop:start_link(stop2),
   {ok, Stop3} = stop:start_link(stop3),
-  {ok, Line1} = line:start_link(1, [Stop1, 5, Stop2, 5, Stop3], bus),
+  Line = line_supervisor:start_line(1, [Stop1, 5, Stop2, 5, Stop3], bus),
 
-  ?assertEqual(line:?GET_NUMBER(Line1), 1),
+  ?assertEqual(line:?GET_NUMBER(Line), 1),
 
   stop:stop(Stop1),
   stop:stop(Stop2),
   stop:stop(Stop3),
-  line:stop(Line1).
+  line_supervisor:stop_line(Line).
 
 get_target_test() ->
+  line_supervisor:start_link(),
   {ok, Stop1} = stop:start_link(stop1),
   {ok, Stop2} = stop:start_link(stop2),
   {ok, Stop3} = stop:start_link(stop3),
@@ -207,7 +215,7 @@ get_target_test() ->
   Dur2_3 = 11,
   Dur3_4 = 13,
   Dur4_5 = 17,
-  {ok, Line} = line:start_link(1, [Stop1, Dur1_2, Stop2, Dur2_3, Stop3, Dur3_4, Stop4, Dur4_5, Stop5], bus),
+  Line = line_supervisor:start_line(1, [Stop1, Dur1_2, Stop2, Dur2_3, Stop3, Dur3_4, Stop4, Dur4_5, Stop5], bus),
 
   ?assertEqual(line:?GET_TARGET(Line, Stop1, Stop3), Stop5),
   ?assertEqual(line:?GET_TARGET(Line, Stop5, Stop3), Stop1),
@@ -219,4 +227,4 @@ get_target_test() ->
   stop:stop(Stop3),
   stop:stop(Stop4),
   stop:stop(Stop5),
-  line:stop(Line).
+  line_supervisor:stop_line(Line).

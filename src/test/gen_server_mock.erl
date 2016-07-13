d5ee43e30b0e65a6fc908b2bdec3abda0ebd5efa
Johan W-Schützer <johan.dss@gmail.com>
2016-07-13 08:42:56 +0200

    Adapted one test function in vehicle to tagged pids

--- src/test/gen_server_mock.erl
+++ src/test/gen_server_mock.erl
@@ -4,7 +4,9 @@
 -include_lib("eunit/include/eunit.hrl").
 
 %% Public API
--export([ start_link/3
+-export([ start_link/2
+        , start_global/2
+        , start_link/3
         , start_global/3
         , stop/1
         , state/1
@@ -24,12 +26,19 @@
 
 %% Public API
 
+start_link(Id, Type) ->
+  gen_server:start_link(?MODULE, #gen_server_mock_state{module=Module, id=Id, type=Type}, []).
+
+start_global(Id, Type) ->
+  gen_server:start_link({global, Id}, ?MODULE, #gen_server_mock_state{module=Module, id=Id, type=Type}, []).
+
 start_link(Module, Id, Type) ->
   {ok, Pid} = gen_server:start_link(?MODULE, #gen_server_mock_state{module=Module, id=Id, type=Type}, []),
   ?ADDRESS(Module).
 
 start_global(Module, Id, Type) ->
-  gen_server:start_link({global, Id}, ?MODULE, #gen_server_mock_state{module=Module, id=Id, type=Type}, []).
+  {ok, Pid} = gen_server:start_link({global, Id}, ?MODULE, #gen_server_mock_state{module=Module, id=Id, type=Type}, []),
+  ?ADDRESS(Module).
 
 stop(Pid) ->
   gen_server:call(Pid, stop).

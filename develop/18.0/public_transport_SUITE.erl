1c000aecdb0db1d00653655c521fbe3232eedd0c
Travis <travis@travis-ci.org>
2016-07-14 16:34:05 +0000

    Travis build 160 pushed to gh-pages

--- develop/18.0/public_transport_SUITE.erl
+++ develop/18.0/public_transport_SUITE.erl
@@ -8,10 +8,4 @@ all() -> [test1].
 
 test1(_Config) ->
   {ok, _PublicTransportSupervisor} = public_transport_supervisor:start_link(),
-  {Route, Dur} = public_transport:?GET_ROUTE(a, o),
-  Ids = lists:map(fun({L1,S2,S3}) ->
-                    LS1 = stop:state(L1),
-                    SS2 = stop:state(S2),
-                    SS3 = stop:state(S3),
-                    {LS1#line_state.number, SS2#stop_state.id, SS3#stop_state.id}
-                  end, Route).
+  {Route, Dur} = public_transport:?GET_ROUTE(a, o).

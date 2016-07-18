5380aae8dd77b5925093a6539b6c660d2045742e
Travis <travis@travis-ci.org>
2016-07-18 11:29:31 +0000

    Travis build 185 pushed to gh-pages

--- develop/18.2/public_transport_SUITE.erl
+++ develop/18.2/public_transport_SUITE.erl
@@ -8,4 +8,4 @@ all() -> [test1].
 
 test1(_Config) ->
   {ok, _PublicTransportSupervisor} = public_transport_supervisor:start_link(),
-  {Route, Dur} = public_transport:?GET_ROUTE(a, o).
+  public_transport:?GET_ROUTE(a, o).

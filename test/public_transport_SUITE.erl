40a96d42fbeb214799546da3fc231b94bd46133a
Johan W-Schützer <johan.dss@gmail.com>
2016-07-21 07:47:29 +0200

    Clearer init in pt. Let logger finish before breaking test

--- test/public_transport_SUITE.erl
+++ test/public_transport_SUITE.erl
@@ -1,14 +1,29 @@
 -module(public_transport_SUITE).
 -include("public_transport.hrl").
+-include("logger.hrl").
 -include_lib("common_test/include/ct.hrl").
 
--export([all/0, test1/1]).
+-export([ all/0
+        , init_per_testcase/2
+        , end_per_testcase/2]).
+-export([test1/1]).
 
 all() -> [test1].
 
-test1(_Config) ->
+init_per_testcase(_TestCase, Config) ->
   put(id, test),
   put(module, "test"),
   logger:start_link("log"),
   {ok, PublicTransportSupervisor} = public_transport_supervisor:start_link(),
+  Config.
+
+end_per_testcase(_TestCase, Config) ->
+  %% Let logger finish
+  receive
+    after 1000 ->
+      ok
+  end,
+  Config.
+
+test1(_Config) ->
   public_transport:?GET_ROUTE(a, o).
